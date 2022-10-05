6d4d7cec1739a5566e8e51d85932076b6f766634
Simon-Olivier Desautels <simon-olivier.desautels@bleuet.design>
2022-10-05 11:38:18 -0400

    finished part 3

--- poll/polls/views.py
+++ poll/polls/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello world. Polls moment")
+from .models import Choice, Question
+
+class QuestionListView(generic.ListView):
+    model = Question
+    template_name = "polls/index.html"
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class QuestionDetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsDetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {'question': question, 'error_message': "You didn't select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
